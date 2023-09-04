fbd019c0642fd85ac5b8dca6c7f5a3994c33d4af
HanBuKui <1040069852@qq.com>
2023-09-04 23:07:30 +0800

    服务端开发 | 员工分页查询 | PageHelper+配置消息转换器使Data转为正确格式JSON(方法二:@JsonFormat)

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -1,10 +1,13 @@
 package com.sky.config;
 
 import com.sky.interceptor.JwtTokenAdminInterceptor;
+import com.sky.json.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -15,6 +18,8 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.List;
 
 /**
  * 配置类，注册web层相关组件
@@ -67,5 +72,21 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+
+    /**
+    *@Description: 扩展消息转换器(java对象与json相互转换) @RequestBody 和 @ResponseBody就是基于Message Converter实现的  此处是为了扩展Spring MVC的消息转换器，
+    *@Param: [converters]
+    *@return: void
+    */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // 创建一个消息转换器对象
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        // 需要为 消息转换器 设置一个 对象转换器， 其可以将 java对象 序列化为 json数据
+        converter.setObjectMapper(new JacksonObjectMapper());
+        // 将自己的消息转换器 加入到容器中  0代表将我们的convert放到最前面，可以优先使用（converts里面默认自带8个消息转换器）
+        converters.add(0,converter);
     }
 }
--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,6 +1,8 @@
 package com.sky.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -17,4 +19,10 @@ public interface EmployeeMapper extends BaseMapper<Employee> {
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
+    /**
+    *@Description: 分页查询
+    *@Param: [employeePageQueryDTO]
+    *@return: com.github.pagehelper.Page<com.sky.entity.Employee>
+    */
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
